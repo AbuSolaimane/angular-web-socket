6dd96968c20c344755ab1763c7bc08999f891007
FADILI EL MOSTAFA <mustafa.fadili@dakilab.fr>
2022-06-10 10:33:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StompService} from "./services/stomp.service";
+import {HttpClient} from "@angular/common/http";
+
+export interface Message {
+  type: string,
+  message: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kafka-websocket-client';
+export class AppComponent implements OnInit{
+  content = '';
+  received: Message[] = [];
+  sent: Message[] = [];
+
+  constructor(private stompService: StompService, private httpClient: HttpClient) {}
+
+  sendMsg() {
+    this.httpClient.post<Message>('http://localhost:8080/kafka/readDataFromWebUi',
+      { type: "info", message: this.content }).subscribe();
+  }
+
+  ngOnInit(): void {
+    this.stompService.subscribe("/topic/message", (data: Message) => console.log(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {StompService} from "./services/stomp.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+    ],
+  providers: [StompService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
